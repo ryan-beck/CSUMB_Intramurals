43441d74022a5e8bd41f8d2701e132431d90e03a
Ryan Beck <rbeck@csumb.edu>
2021-02-24 13:12:33 -0800

    react set up

---
+++ IntramuralsSite/reactapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <h1> I made a react thing with snek </h1>
+
+    </div>
+  );
+}
+
+export default App;
