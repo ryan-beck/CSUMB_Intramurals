4e80514fc5b7ba0be9715b5b5307097af36ba6e0
Ryan Beck <rbeck@csumb.edu>
2021-03-22 17:25:18 -0700

    example endpoint

--- IntramuralsSite/reactapp/src/pages/index.js
+++ IntramuralsSite/reactapp/src/pages/index.js
@@ -1,10 +1,33 @@
 import React from "react";
+import axios from 'axios';
+
+
+function handleSubmit(event) {
+	const text = document.querySelector('#char-input').value
+  
+	axios
+	  .get(`http://localhost:8000/char_count?text=${text}`).then(({data}) => {
+		console.log(`${data.count}`)
+	  })
+	  .catch(err => console.log(err))
+  }
+
 
 const MainPage = () => {
+	// return (
+	// 	<div>
+	// 		<h3>welcome to main</h3>
+	// 		<small>feed will be here soon</small>
+	// 	</div>
+	// );
 	return (
+		<div className="App">
 		<div>
-			<h3>welcome to main</h3>
-			<small>feed will be here soon</small>
+			<label htmlFor='char-input'>How many characters does</label>
+			<input id='char-input' type='text' />
+			<button onClick={handleSubmit}>have?</button>
+		</div>
+	
 		</div>
 	);
 }
