08dec9b493789903106083925287a2318b2ef59e
Ryan Beck <rbeck@csumb.edu>
2021-04-12 13:38:06 -0700

    added str methods to models

--- IntramuralsSite/main/models.py
+++ IntramuralsSite/main/models.py
@@ -6,10 +6,18 @@ class Account(models.Model):
     photo_url = models.TextField()
     is_admin = models.BooleanField(default=False) 
 
+    def __str__(self):
+        return self.email
+    
+
 class Sport(models.Model):
     sport_name = models.TextField()
     logo_url = models.TextField()
     is_active = models.BooleanField()
+
+    def __str__(self):
+        return self.sport_name
+    
 
 class League(models.Model):
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
@@ -17,13 +25,22 @@ class League(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     # registration dates:
+    # TODO: maybe add constraints on these dates
     reg_start_date = models.DateField()
     reg_end_date = models.DateField()
+
+    def __str__(self):
+        return self.league_name
+    
 
 class Team(models.Model):
     team_name = models.TextField(default="My Team")
     league = models.ForeignKey(League, on_delete=models.CASCADE)
     players = models.ManyToManyField(Account)
+
+    def __str__(self):
+        return self.team_name
+    
 
 class Game(models.Model):
     league = models.ForeignKey(League, on_delete=models.CASCADE)
--- IntramuralsSite/main/views.py
+++ IntramuralsSite/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Account, Team
 
 def index(request):
     return HttpResponse("Index page for website")
@@ -11,3 +14,25 @@ def char_count(request):
     text = request.GET.get("text", "")
 
     return JsonResponse({"count": len(text)})
+
+@api_view(['POST'])
+def join_team(request):
+    user_id = request.data['user_id']
+    team_id = request.data['team_id']
+    user_account = Account.objects.get(id=user_id)
+    team = Team.objects.get(id=team_id)
+    team.players.add(user_account)
+    team.save()
+    return HttpResponse('Successfully joined team')
+
+    # print("here 1")
+    # if request.method == "POST":
+    #     print("here 2")
+    #     user_id = request.POST.get("user_id")
+    #     team_id = request.POST.get("team_id")
+    #     user_account = Account.objects.get(id=user_id)
+    #     team = Team.objects.get(id=team_id)
+    #     team.players.add(user_account)
+    #     team.save()
+    #     return HttpResponse('Successfully joined team')
+    # return HttpResponse('Failed')
