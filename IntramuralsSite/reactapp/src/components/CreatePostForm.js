6489f80c4708ab02276e65706a72ab853bd7995a
casscabrera <cass.cabrera99@gmail.com>
2021-05-03 16:05:28 -0700

    adding images to firebase and django

--- IntramuralsSite/reactapp/src/components/CreatePostForm.js
+++ IntramuralsSite/reactapp/src/components/CreatePostForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios"; 
+import { storage } from '../firebase';
 
 class CreatePostForm extends Component {
 
@@ -8,13 +9,16 @@ class CreatePostForm extends Component {
 
         this.state = {
             owner: props.userId,
-            postText: ""
+            postText: "",
+            image: null,
+            mediaUrl: ""
         }
 
         this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.postToDjango = this.postToDjango.bind(this);
         this.submitHandler = this.submitHandler.bind(this);
     }
-
 
     onChangeHandler(event) {
         this.setState(
@@ -24,19 +28,30 @@ class CreatePostForm extends Component {
         )
     }
 
-    submitHandler(event) {
-        event.preventDefault();
+    handleChange(event) {
+        if(event.target.files[0]) {
+            this.setState({
+                image: event.target.files[0]
+            });
+        }
+    }
+
+    postToDjango() {
+        // get current date and time in correct format
         var today = new Date(),
         date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate(),
         time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
         var today = date+' '+time;
-        
+
+
+        console.log(this.state.mediaUrl);
+        // add post to django database
         axios({
             method:'post', 
             url: 'http://localhost:8000/api/createPost/', 
             data: {
               text: this.state.postText,
-              media_url: "",
+              media_url: this.state.mediaUrl,
               owner: this.state.owner,
               posted_date: today
             }
@@ -44,7 +59,35 @@ class CreatePostForm extends Component {
         .then(({data}) => {
             console.log(data);
         });
-        
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+
+        if(this.state.image) {
+            var uploadTask = storage.ref(`images/${this.state.image.name}`).put(this.state.image);
+            uploadTask.on(
+                "state_changed",
+                snapshot => {},
+                error => {
+                    console.log(error);
+                },
+                () => {
+                    storage
+                    .ref("images")
+                    .child(this.state.image.name)
+                    .getDownloadURL()
+                    .then(url => {
+                        this.setState({
+                            mediaUrl: url
+                        });
+                        this.postToDjango();
+                    })
+                }
+            );
+        } else {
+            this.postToDjango();
+        }     
     }
     
 
@@ -55,7 +98,7 @@ class CreatePostForm extends Component {
                 <form onSubmit={this.submitHandler}>
                     <label>Caption</label> <br/>
                     <textarea name="postText" rows="5" cols="50" value={this.state.postText} onChange={this.onChangeHandler}/> <br/>
-                    <input type="file" name="myfile" accept="image/*"/>
+                    <input type="file" name="myfile" accept="image/*" onChange={this.handleChange}/>
                     <input type="submit" value="Submit"/>
                 </form>
             </div>
